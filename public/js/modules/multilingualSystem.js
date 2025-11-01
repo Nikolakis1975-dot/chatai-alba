f3f5045a83a4e13e8b535be734d7fd8553290f30
Nikolakis1975-dot <cimicimi1975@gmail.com>
2025-11-01 16:08:34 +0200

    Create multilingualSystem.js
    Perditsova modulin translate

---
+++ public/js/modules/multilingualSystem.js
@@ -0,0 +1,17 @@
+// Krijo /js/modules/multilingualSystem.js
+class MultilingualSystem {
+    constructor() {
+        this.languages = {
+            'sq': 'Shqip',
+            'en': 'English', 
+            'de': 'Deutsch',
+            'it': 'Italiano',
+            'fr': 'Français'
+        };
+        this.currentLang = 'sq';
+    }
+    
+    async translateDeclaration() {
+        // Implemento përkthimin automatik ose manual
+    }
+}
